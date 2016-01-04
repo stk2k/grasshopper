39a23388c63738631ff4d3ce93ffc282cc89a4b9
stk2k <stk2k@sazysoft.com>
2016-01-04 11:58:38 +0900

    Grasshopper class add

---
+++ src/Grasshopper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stk2k
+ * Date: 2016/01/04
+ * Time: 11:43
+ */
+
+namespace Grasshopper;
+
+
+class Grasshopper
+{
+    /**
+     * Constructs grasshopper object
+     *
+     * @param array $options
+     */
+    public function __construct($options) {
+
+    }
+}
